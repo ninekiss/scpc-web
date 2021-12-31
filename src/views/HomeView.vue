72c05cca3a1ebee483f9b46188ffeebcb9b7c810
lankeo <aodocoo@gmail.com>
2022-01-01 00:51:31 +0800

    style: update page style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,3 +5,13 @@ import TheWelcome from '@/components/TheWelcome.vue'
 <template>
   <div class="about"><h1>Coming soon...</h1></div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
