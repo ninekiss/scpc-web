3911e6614ee73a1ead6af491932978385876fc12
兰克奥 <44481208+lankeo@users.noreply.github.com>
2022-01-02 22:13:09 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
-  <div class="about"><h1>施工中 Coming soon...</h1></div>
+  <div class="about"><h1>Coming soon...</h1></div>
 </template>
 
 <style>
